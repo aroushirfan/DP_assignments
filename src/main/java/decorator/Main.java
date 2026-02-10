09cb0d2695ae0800e77e0b3d5f07a6aeca239fc2
Aroush <aroushi@metropolia.fi>
2026-02-10 19:34:23 +0200

    decorator

--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -3,16 +3,19 @@ package decorator;
 public class Main {
     public static void main(String[] args) {
         Printer printer;
+        System.out.println("--- Basic Printer ---");
         printer = new BasicPrinter();
         printer.print("Decorator Assignment");
+        System.out.println();
 
+        System.out.println("--- XML Printer ---");
         printer = new XMLPrinter(new BasicPrinter());
         printer.print("Decorator Assignment");
+        System.out.println();
+
+        System.out.println("--- Encrypted Printer ---");
         printer = new EncryptedPrinter(new BasicPrinter());
         printer.print("Decorator Assignment");
-        printer = new EncryptedPrinter(new XMLPrinter(new BasicPrinter()));
-        printer.print("Decorator Assignment");
-        printer= new XMLPrinter(new EncryptedPrinter(new BasicPrinter()));
-        printer.print("Decorator Assignment");
+        System.out.println();
     }
 }
